9952e0d11dc4d87c9d6b0a970bf86ba2641cb93e
GoldenEagle74 <mshchannikov@yandex.ru>
2023-10-10 16:14:24 +0300

    added if name main for test

--- src/neural_network/bart_model.py
+++ src/neural_network/bart_model.py
@@ -35,11 +35,13 @@ def summarize_text(input_text, max_input_length=1024, max_summary_length=None):
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
 
-# Пример использования:
-input_text = """All-in-one desktop computers are a great space-saving solution for folks who work at home or in a cramped office. But what if you want to work from a coffee shop? Laptop's just don't offer a big-screen experience, so HP has made the AIO mobile with the Envy Move.
+if __name__ == "__main__":
 
-Though I work from home, I use a laptop rather than a desktop PC – but I have it cabled to a computer monitor over HDMI because the bigger screen makes for more comfortable viewing.
+    # Пример использования:
+    input_text = """All-in-one desktop computers are a great space-saving solution for folks who work at home or in a cramped office. But what if you want to work from a coffee shop? Laptop's just don't offer a big-screen experience, so HP has made the AIO mobile with the Envy Move.
 
-HP has combined the portable convenience of a laptop with power of an all-in-one PC by building in an 83-Wh Li-ion battery for up to four hours away from the wall outlet. The AIO supports fast-charging for a top-up to 50% capacity in around 45 minutes. There's a built-in carry handle, two swivel kickstand feet, and it even sports a pocket at the back to carry the included Bluetooth keyboard (a mouse is an optional extra)."""
-summarized_text = summarize_text(input_text)
-print(summarized_text)
+    Though I work from home, I use a laptop rather than a desktop PC – but I have it cabled to a computer monitor over HDMI because the bigger screen makes for more comfortable viewing.
+
+    HP has combined the portable convenience of a laptop with power of an all-in-one PC by building in an 83-Wh Li-ion battery for up to four hours away from the wall outlet. The AIO supports fast-charging for a top-up to 50% capacity in around 45 minutes. There's a built-in carry handle, two swivel kickstand feet, and it even sports a pocket at the back to carry the included Bluetooth keyboard (a mouse is an optional extra)."""
+    summarized_text = summarize_text(input_text)
+    print(summarized_text)
