083d9fb8d457ee1621485da006072d7ec23ee557
GoldenEagle74 <mshchannikov@yandex.ru>
2023-10-14 18:06:52 +0300

    Реорганизован код news_parser.py для повышения читаемости и разделения функционала. Добавлена новая функция parse_news для парсинга новостей и удаления нежелательных разделов. Обновлена функция get_latest_news для использования parse_news вместо повторного парсинга. Улучшена обработка ошибок при доступе к веб-сайту.

--- src/data_processing/news_parser.py
+++ src/data_processing/news_parser.py
@@ -2,8 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-# Функция для удаления секции "More on Twitter" и "Share This Article"
-def remove_unwanted_sections(soup):
+# Функция для парсинга новости
+def parse_news(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Не удалось получить доступ к странице: {url}")
+        return None
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Функция для удаления секции "More on Twitter" и "Share This Article"
     for unwanted_section in soup.find_all('p'):
         if "More on" in unwanted_section.get_text():
             unwanted_section.extract()
@@ -12,47 +20,31 @@ def remove_unwanted_sections(soup):
     if tracking_section:
         tracking_section.extract()
 
-# Функция для парсинга новости
-def parse_news(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        remove_unwanted_sections(soup)
-        title = soup.find('h1', class_='text-5xl').text
-        article_content = soup.find('main', class_='post-content')
-        paragraphs = article_content.find_all('p')
-        article_text = '\n'.join([p.text for p in paragraphs])
-        news_data = {
-            "Title": title,
-            "Article Content": article_text,
-            "Link": url,
-        }
-        return news_data
-    else:
-        print("Failed to retrieve the webpage.")
-        return None
+    title = soup.find('h1', class_='text-5xl').text
+    article_content = soup.find('main', class_='post-content')
+    paragraphs = article_content.find_all('p')
+    article_text = '\n'.join([p.text for p in paragraphs])
+
+    news_data = {
+        "Title": title,
+        "Article Content": article_text,
+        "Link": url,
+    }
+    return news_data
 
 # Функция для получения последних 5 новостей
 def get_latest_news():
     url = 'https://futurism.com/latest'
     response = requests.get(url)
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        news_items = soup.find_all('a', class_='block')
-        latest_news = news_items[:5]
-        news_list = []
-
-        for news in latest_news:
-            link = 'https://futurism.com' + news.get('href')
-            news_data = parse_news(link)
-            if news_data:
-                news_list.append(news_data)
-
-        return news_list
-    else:
+    if response.status_code != 200:
         print("Не удалось получить доступ к сайту.")
         return None
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    news_items = soup.find_all('a', class_='block')[:5]
+
+    news_list = [parse_news('https://futurism.com' + news.get('href')) for news in news_items]
+    return [news_data for news_data in news_list if news_data]
 
 if __name__ == '__main__':
     latest_news = get_latest_news()
