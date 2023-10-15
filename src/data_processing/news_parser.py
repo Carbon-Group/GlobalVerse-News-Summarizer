5e664baba91e2d39259bb2a8a151f6ec7758799c
GoldenEagle74 <mshchannikov@yandex.ru>
2023-10-15 11:07:58 +0300

    Реорганизован код news_parser.py для повышения читаемости и разделения функционала. Добавлена новая функция parse_news для парсинга новостей и удаления нежелательных разделов. Обновлена функция get_latest_news для использования parse_news вместо повторного парсинга. Улучшена обработка ошибок при доступе к веб-сайту.

--- src/data_processing/news_parser.py
+++ src/data_processing/news_parser.py
@@ -1,16 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-import logging
 
-# Логирование
-logging.basicConfig(filename='news_parser.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# Функция для парсинга новости
+def parse_news(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Не удалось получить доступ к странице: {url}")
+        return None
 
-# Конфигурация
-URL = 'https://futurism.com/latest'
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-# Функция для удаления секции "More on Twitter" и "Share This Article"
-def remove_unwanted_sections(soup):
+    # Функция для удаления секции "More on Twitter" и "Share This Article"
     for unwanted_section in soup.find_all('p'):
         if "More on" in unwanted_section.get_text():
             unwanted_section.extract()
@@ -19,45 +20,36 @@ def remove_unwanted_sections(soup):
     if tracking_section:
         tracking_section.extract()
 
-# Функция для парсинга новости
-def parse_news(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
-        remove_unwanted_sections(soup)
-        title = soup.find('h1', class_='text-5xl').text
-        article_content = soup.find('main', class_='post-content')
-        paragraphs = article_content.find_all('p')
-        article_text = '\n'.join([p.text for p in paragraphs])
-        news_data = {
-            "Title": title,
-            "Article Content": article_text,
-            "Link": url,
-        }
-        return news_data
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Failed to retrieve the webpage: {e}")
+    title = soup.find('h1', class_='text-5xl').text
+    article_content = soup.find('main', class_='post-content')
+    paragraphs = article_content.find_all('p')
+    article_text = '\n'.join([p.text for p in paragraphs])
+
+    news_data = {
+        "Title": title,
+        "Article Content": article_text,
+        "Link": url,
+    }
+    return news_data
+
+# Функция для получения последних 5 новостей
+def get_latest_news():
+    url = 'https://futurism.com/latest'
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("Не удалось получить доступ к сайту.")
         return None
 
-# Функция для получения последних новостей
-def get_latest_news(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
-        news_items = soup.find_all('a', class_='block')[:5]
+    soup = BeautifulSoup(response.text, 'html.parser')
+    news_items = soup.find_all('a', class_='block')[:5]
 
-        news_list = [parse_news('https://futurism.com' + news.get('href')) for news in news_items]
-        return [news_data for news_data in news_list if news_data]
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Failed to retrieve the webpage: {e}")
-        return None
+    news_list = [parse_news('https://futurism.com' + news.get('href')) for news in news_items]
+    return [news_data for news_data in news_list if news_data]
 
-if __name__ == '__main':
-    latest_news = get_latest_news(URL)
+if __name__ == '__main__':
+    latest_news = get_latest_news()
     if latest_news:
         with open('news_data.json', 'w', encoding='utf-8') as json_file:
             json.dump(latest_news, json_file, ensure_ascii=False, indent=4)
     else:
-        logging.error("Не удалось получить доступ к сайту.")
+        print("Не удалось получить доступ к сайту.")
